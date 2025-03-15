a1b6cb711c5ee39862a75609d0cf338bd780aa6a
roro4112 <rohan.the.chicken@gmail.com>
2025-03-14 21:19:42 -0600

    uploaded correct range test

--- JfreeChart_Lab3/src/org/jfree/data/RangeTest.java
+++ JfreeChart_Lab3/src/org/jfree/data/RangeTest.java
@@ -237,6 +237,12 @@ public class RangeTest {
     		fail("When passed a null shift should throw a InvalidParameterError but instead throws:" + e.getMessage());
     	}
     }
+    @Test
+    public void testShiftingZeroWithZeroCrossingFalse() {
+    	expectedRange = new Range(0,0);
+    	exampleRange = new Range(0,0);
+    	assertEquals("AfterShifting range (0,0) by 0 result should be (0,0)",expectedRange,Range.shift(exampleRange,0,false));
+    }
 
     //Expand Section()
     @Test
@@ -279,7 +285,11 @@ public class RangeTest {
     	expectedRange = new Range(-5,5);
     	assertEquals("After expanding the range -1,1 by 2,2 the returned range should be -5,5",expectedRange,Range.expand(exampleRange, 2, 2));
     }
-    
+    @Test 
+    public void testLowerSurpassingUpperBoundWithExpand() {
+    	expectedRange = new Range(1,3);
+    	assertEquals("After expanding the range -1,1 by -2,0 the returned range should be 1,3",expectedRange,Range.expand(exampleRange, -2,0));
+    }
     @Test
     public void testExceptionWhenNullRangePassedForExpand() {
     	try {
@@ -290,7 +300,190 @@ public class RangeTest {
     		fail("When passed a null shift should throw a InvalidParameterError but instead throws:" + e.getMessage());
     	}
     }
+    //to string test
+    @Test
+    public void testValidRangeToString() {
+    	assertEquals("the range(-1,1) should be expressed in string form as Range[-1.0,1.0]","Range[-1.0,1.0]", exampleRange.toString());
+    }
+    //isNanRange test
+    @Test
+    public void testNanRangeInIsNanRange() {
+    	exampleRange = new Range(Double.NaN,Double.NaN);
+    	assertTrue("the range(Double.NaN,Double.NaN) should be identified as a nan range",exampleRange.isNaNRange());
+    }
+    
+    @Test
+    public void testValidRangeInIsNanRange() {
+    	assertFalse("the range(-1,1) should be identified as not NaN", exampleRange.isNaNRange());
+    }
+    
+    @Test
+    public void testLowerHalfValidRangeInIsNanRange() {
+    	exampleRange = new Range(0,Double.NaN);
+    	assertTrue("the range(0,Double.NaN) should be identified as a nan range",exampleRange.isNaNRange());
+    }
+    
+    @Test
+    public void testUpperHalfValidRangeInIsNanRange() {
+    	exampleRange = new Range(Double.NaN,0);
+    	assertTrue("the range(Double.NaN,0) should be identified as a nan range",exampleRange.isNaNRange());
+    }
+    //test for scale()
+    @Test (expected = IllegalArgumentException.class)
+    public void testNullRangeInScale() throws IllegalArgumentException{
+    	Range.scale(null, 0);
+    }
+    
+    @Test (expected = IllegalArgumentException.class)
+    public void testInvalidScaleInScale() throws IllegalArgumentException{
+    	Range.scale(exampleRange, -1);
+    }
+    
+    @Test
+    public void testValidScaleInScale() {
+    	expectedRange = new Range(-2,2);
+    	assertEquals("the range (-1,1) scaled by 2 should be (-2,2)",expectedRange,Range.scale(exampleRange,2));
+    }
 
+    //expandToIncludeTests
+    @Test
+    public void testNullRangeInExpandToInclude() {
+    	expectedRange = new Range(1,1);
+    	assertEquals("when passed a null range expandToInclude should return a range including only the include value",expectedRange,Range.expandToInclude(null,1));
+    }
+    
+    @Test
+    public void testValueBelowRangeForExpandToInclude() {
+    	expectedRange = new Range(-2,1);
+    	assertEquals("the range -1,1 should be expanded to -2,1 to include -2",expectedRange,Range.expandToInclude(exampleRange, -2));
+    }
+    
+    @Test
+    public void testValueAboveRangeForExpandToInclude() {
+    	expectedRange = new Range(-1,2);
+    	assertEquals("the range -1,1 should be expanded to -1,2 to include 2",expectedRange,Range.expandToInclude(exampleRange, 2));
+    }
+    
+    @Test
+    public void testValueWithinRangeForExpandToInclude() {
+    	expectedRange = new Range(-1,1);
+    	assertEquals("the range -1,1 shoudl reamin -1,1 to include 0",expectedRange,Range.expandToInclude(exampleRange, 0));
+    }
+    
+  //getCentralValue
+    @Test
+    public void testgetCentralvalue() {
+    	expectedRange = new Range(1, 1);
+    	assertEquals("Central value should be 1",  expectedRange.getCentralValue(), 1.0, 0.000000001d);
+    }
+    //Intersects
+    @Test
+    public void testIntersectdoubleMethodlower() {
+    	expectedRange = new Range(1, 5);
+    	assertTrue("1, 4 should return True", expectedRange.intersects(1.0, 4.0));
+    }
+    @Test
+    public void testIntersectdoubleMethodlowerfalse() {
+    	expectedRange = new Range(1, 5);
+    	assertFalse("1, -1 should return False", expectedRange.intersects(1, -1));
+    }
+    
+    @Test
+    public void testIntersectiondoubleMethodHigh() {
+    	expectedRange = new Range(3, 5);
+    	assertTrue("4, 6 should intersect", expectedRange.intersects(4, 6));
+    }
+    @Test
+    public void testIntersectiondoubleMethodHighFalse() {
+    	expectedRange = new Range(3, 5);
+    	assertFalse("4, -6 shouldnt intersect", expectedRange.intersects(4, -6));
+    }
+    
+    @Test
+    public void testIntersectionRangeMethod() {
+    	exampleRange = new Range(1,3);
+    	expectedRange = new Range(2,3);
+    	assertTrue("Range 1,3 and 2,3 should intersect", exampleRange.intersects(expectedRange));
+    }
+    
+    // test Combine
+    @Test
+    public void testCombineleftNull() {
+    	exampleRange = new Range(1, 3);
+    	CompRange = Range.combine(null, exampleRange);
+    	assertTrue("Expected Null return", exampleRange.equals(CompRange));
+    }
+    @Test
+    public void testCombinerightNull() {
+    	exampleRange = new Range(2,3);
+    	CompRange = Range.combine(exampleRange, null);
+    	assertTrue("Expected Null Return", exampleRange.equals(CompRange));
+    }
+    @Test
+    public void testCombine() {
+    	expectedRange = new Range(1,1);
+    	exampleRange = new Range(0, 2);
+    	CompRange = Range.combine(exampleRange, expectedRange);
+    	
+    	assertTrue("Bounds should equal", exampleRange.equals(CompRange));
+    }
+    //Test combine ignoring NaN
+    @Test
+    public void testCombineNaNleftNull() {
+    	exampleRange = new Range(Double.NaN,Double.NaN);
+    	assertNull("Expected Null return", Range.combineIgnoringNaN(null, exampleRange));
+    }
+    @Test
+    public void testCombineNaNleft() {
+    	exampleRange = new Range(1,1);
+    	CompRange = Range.combineIgnoringNaN(null, exampleRange);
+    	assertTrue("Expected to return 1, l range", exampleRange.equals(CompRange));
+    }
+    
+    @Test
+    public void testCombineNaNRightNull() {
+    	exampleRange = new Range(Double.NaN,Double.NaN);
+    	assertNull("Expected Null return", Range.combineIgnoringNaN(exampleRange, null));
+    }
+    @Test
+    public void testCombineNaNRight() {
+    	exampleRange = new Range(1,1);
+    	CompRange = Range.combineIgnoringNaN(exampleRange, null);
+    	assertTrue("Expected to return 1, l range", exampleRange.equals(CompRange));
+    }
+    @Test
+    public void testCombineNaN() {
+    	exampleRange = new Range(1,5);
+    	expectedRange = new Range(0, 7);
+    	CompRange = Range.combineIgnoringNaN(exampleRange, expectedRange);
+    	assertTrue("should return range 0, 7", expectedRange.equals(CompRange));
+    }
+    @Test
+    public void testCombineNaNBoth() {
+    	exampleRange = new Range(Double.NaN,Double.NaN);
+    	expectedRange = new Range(Double.NaN,Double.NaN);
+    	assertNull("should return null", Range.combineIgnoringNaN(exampleRange, expectedRange));
+    }
+    //Testing illegal argument for constructor
+    @Test(expected = IllegalArgumentException.class)
+    public void testingIllegalConstructor() {
+    	expectedRange = new Range(9, 1);
+    }
+    
+    //testing min and max
+    @Test
+    public void testingMinwithNaN() {
+    	exampleRange = new Range(1, 1);
+    	expectedRange = new Range(Double.NaN, Double.NaN);
+    	CompRange = Range.combineIgnoringNaN(exampleRange, expectedRange);
+    	assertTrue("should return True ", exampleRange.equals(CompRange));
+    }
+    
+    //testing hashcode
+    @Test
+    public void testHashcode() {
+    	assertEquals("The function hashcode should return -31457280 for the range -1,1",-31457280,exampleRange.hashCode());
+    }
     @After
     public void tearDown() throws Exception {
     }
